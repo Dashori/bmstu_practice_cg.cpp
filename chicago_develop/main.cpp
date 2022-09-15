b17d6cbb891766d06979ca731f5d9dd21bd0ba0b
dashori <daahaaa@icloud.com>
2022-09-15 17:56:07 +0300

    fix yml and obj

--- chicago_develop/main.cpp
+++ chicago_develop/main.cpp
@@ -40,7 +40,7 @@ int parseArguments(MainWindow &w, int argc, char *argv[])
         {
             int num = std::stoi(argv[i+1]);
 
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 10; i++)
             {
                 w.on_rotateY_clicked();
 
